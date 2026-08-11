98d61cdb59ca0db7328075cbc344f3817a8acf6a
malkinder singh <malkindersingh919@gmail.com>
2026-08-11 09:18:42 +0530

    new vowel changed

---
+++ conditionalStatements/vowels-check.py
@@ -0,0 +1,10 @@
+char = input("enter your character")
+
+
+
+match(char.lower()):
+    case "a" | "i" | "o" | "u" | "e":
+        print(char , "is a vowel")
+    case _:
+        print(char , "not a vowels")
+    
